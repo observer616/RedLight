f44bfab32c12c848c5dd9b8908cd3fb5943512da
ray <1099063957@qq.com>
2022-05-23 21:45:09 +0800

    finish Admin-Comment model

--- src/main/java/com/ryan/redlight/web/admin/AdminCommentController.java
+++ src/main/java/com/ryan/redlight/web/admin/AdminCommentController.java
@@ -1,6 +1,7 @@
 package com.ryan.redlight.web.admin;
 
 import com.github.pagehelper.PageInfo;
+import com.ryan.redlight.entity.Admin;
 import com.ryan.redlight.entity.Comment;
 import com.ryan.redlight.entity.vo.CommentVo;
 import com.ryan.redlight.entity.vo.Msg;
@@ -8,12 +9,14 @@ import com.ryan.redlight.interceptor.AdminCheck;
 import com.ryan.redlight.service.CommentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpSession;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -31,7 +34,7 @@ public class AdminCommentController {
 
     @AdminCheck
     @RequestMapping(value = "/get/list")
-    public String commentList(@RequestParam(value = "pageNum",required = false, defaultValue = "1") Integer pageNum,
+    public String commentList(@RequestParam(value = "pageNum", required = false, defaultValue = "1") Integer pageNum,
                               Model model) {
         PageInfo<CommentVo> commentVoPageInfo = commentService.selectAll(pageNum);
         List<CommentVo> commentVoList = commentVoPageInfo.getList();
@@ -43,10 +46,22 @@ public class AdminCommentController {
 
     // 回复留言
     @AdminCheck
-    @GetMapping(value = "/update")
-    public String update(@ModelAttribute CommentVo commentVo,
+    @PostMapping(value = "/update")
+    public String update(@RequestParam(value = "commentId")Integer commentId,
+                         @RequestParam(value = "replyInfo") String replyInfo,
+                         HttpSession httpSession,
                          RedirectAttributes redirectAttributes) {
-        Msg msg = commentService.updateByPrimaryKeySelective(new Comment(commentVo));
+        Admin admin = (Admin) httpSession.getAttribute("adminInfo");
+        // 构造comment
+        Comment comment = new Comment();
+        comment.setCommentId(commentId);
+        comment.setIsReplied((byte) 1);
+        comment.setReplyId(admin.getAdminId());
+        comment.setReplyTime(new Date());
+        comment.setReplyInfo(replyInfo);
+        //todo
+        System.out.println(comment);
+        Msg msg = commentService.updateByPrimaryKeySelective(comment);
         redirectAttributes.addFlashAttribute("msg", msg);
         return "redirect:/admin/comments/get/list";
     }
