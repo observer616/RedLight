e8bda9d1643cb65e8e0641047d0ec2a0d23daf60
ray <1099063957@qq.com>
2022-05-25 21:22:53 +0800

    ADs at home page

--- src/main/java/com/ryan/redlight/web/client/HomeController.java
+++ src/main/java/com/ryan/redlight/web/client/HomeController.java
@@ -1,15 +1,33 @@
 package com.ryan.redlight.web.client;
 
+import com.github.pagehelper.PageInfo;
+import com.ryan.redlight.entity.House;
+import com.ryan.redlight.service.HouseService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * @author Ryan
  */
 @Controller
 public class HomeController {
+    final
+    HouseService houseService;
+
+    public HomeController(HouseService houseService) {
+        this.houseService = houseService;
+    }
+
     @RequestMapping(value = "/home")
-    public String homePage() {
+    public String homePage(Model model) {
+        // 用于广告轮播
+        PageInfo<House> housePageInfo = houseService.selectList(1, null);
+        List<House> houseList = housePageInfo.getList();
+        model.addAttribute("housePageInfo", housePageInfo);
+        model.addAttribute("houseList", houseList);
         return "client/home";
     }
 }
